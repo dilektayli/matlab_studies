fa8919e3640a075bd5bf9412c5a368a52dae378f
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-13 19:54:23 +0300

    Create A_002_string.m

---
+++ A_002_string.m
@@ -0,0 +1,17 @@
+name = "dilek";
+surname = 'tayli';
+
+whos name
+whos surname
+
+
+name2 = "dilekkkk";
+surname2 = 'tayliiii';
+
+whos name2
+whos surname2
+
+number1 = '4';
+number2 =4;
+whos number1
+whos number2
