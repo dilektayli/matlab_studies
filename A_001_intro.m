99968edfc54840ee2cc392dc8c7877800338a05f
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-13 19:53:24 +0300

    Create A_001_intro.m

---
+++ A_001_intro.m
@@ -0,0 +1,43 @@
+number1 = 2;
+number2 = 7;
+number3 = 10.4;
+
+whos number3
+number4= 19.1;
+
+number5 = single(19.1);
+whos number5
+
+
+number6 = int8(10);
+whos number6
+
+number7 = int8(-3555);
+whos number7
+
+
+number8  = int16(18);
+whos number8
+
+
+number9  = int32(18);
+whos number9
+
+number10  = int64(18);
+whos number10
+
+
+number11 = int8(-4);
+whos number11
+
+number12 = uint8(number11);
+whos number12
+
+number13 = uint16(number11);
+whos number13
+
+number14 = uint32(number11);
+whos number14
+
+number15 = uint64(number11);
+whos number15
