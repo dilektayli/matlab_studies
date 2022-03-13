fb227833b296e18547a4ea3d2ab710d3704ffb6e
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-13 19:56:10 +0300

    Create A_005_matrixAndVector.m

---
+++ A_005_matrixAndVector.m
@@ -0,0 +1,15 @@
+vector1 = [1 2 3 4];
+whos vector1
+
+vector2 = [1,2,3,4];
+whos vector2
+
+matrix1 = [3,2,0;1,6,3;5,7,2];
+whos matrix1
+
+matrix2 = [9 13 5 2; 1 11 7 6; 3 7 4 1; 6 0 7 10];
+whos matrix2
+
+matrix3 = [3,2,2;0,4,6;0,0,5];
+whos matrix3
+
