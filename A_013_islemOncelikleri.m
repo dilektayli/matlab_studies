807d794e8657ec8c1c3a190ec6bd636800e9aec1
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-21 19:20:15 +0300

    Create A_013_islemOncelikleri.m

---
+++ A_013_islemOncelikleri.m
@@ -0,0 +1,17 @@
+% işlem öncelikleri
+% parantezler
+% üs alma işlemleri
+% çarpma bölme
+% toplama çıkarma
+
+sayi1 = 4;
+sayi2 = 5;
+sayi3 = 2;
+
+sonuc1 = sayi1*sayi2+sayi3;
+sonuc2 = sayi1*(sayi2+sayi3);
+sonuc3 = sayi3 - sayi1/sayi2;
+sonuc4 = sayi2/(sayi1-sayi2);
+
+sayi4 = -3;
+sonuc5 = sayi1- (sayi1+sayi3^sayi1)*sayi4^sayi3;
