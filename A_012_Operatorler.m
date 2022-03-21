c8235fdde9affee40ebd068905ab8b8ea6b215c8
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-21 19:14:45 +0300

    Create A_012_Operatorler.m

---
+++ A_012_Operatorler.m
@@ -0,0 +1,26 @@
+sayi1  =1;
+sayi2 = 5;
+sayi3 = 7;
+toplam1 = 4+5;
+toplam2 = sayi3+2;
+toplam3 = plus(sayi2,sayi1);
+
+fark1 = minus(sayi1,sayi3);
+fark2 = -sayi3;
+fark3 = uminus(sayi3);
+
+carpim1 = times(fark2,fark3);
+
+bolum1 = sayi3 / sayi1;
+bolum2 = sayi3 \ sayi3;
+
+bolum3 = mrdivide(sayi3,sayi2);
+bolum4 = mldivide(sayi3,sayi2);
+
+karesi1 = sayi2^2;
+karesi2 = sayi2*sayi2;
+
+kup2 = sayi2*sayi2*sayi2;
+kup3 = sayi2 ^3;
+
+sonuc = power(sayi3,4);
