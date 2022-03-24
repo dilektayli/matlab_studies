74c2af98a60194534d8f6e6b5a8d9ce2359c1843
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-24 23:11:39 +0300

    Create A_018_matrixElemanlar.m

---
+++ A_018_matrixElemanlar.m
@@ -0,0 +1,15 @@
+matris1 = [1,2,3;4,5,6;7,8,9];
+matris1(9)
+matris1(3,3)
+matris1(1,1) = matris1(1,1) + 11
+matris1
+
+
+kelime = 'merhaba';
+kelime(1)
+kelime(2)
+kelime(3)
+
+
+kelime2 = ['merhaba','naber']
+kelime2 = ['merhaba ','naber']
