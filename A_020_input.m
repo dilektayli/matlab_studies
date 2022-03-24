a49c189e7703fd9018a0cf5f7d49f6c4d75e9916
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-24 23:18:35 +0300

    Create A_020_input.m

---
+++ A_020_input.m
@@ -0,0 +1,16 @@
+clc;
+clear all;
+close all;
+
+yil = input('Doğum tarini gir: ');
+yas = 2022 - yil
+
+
+kilo = input("kilonu gir: ");
+kilo
+
+ad = input('Adin: ');
+soyad = input('Soyadin ');
+asoyad = [ad '' soyad]
+
+yaslar = input("yaşaları gir")
