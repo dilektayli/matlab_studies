8213f05eaaa94d12174139e843b28babfbf65139
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-26 09:26:54 +0300

    Create A_022_IfElse.m

---
+++ A_022_IfElse.m
@@ -0,0 +1,37 @@
+clc;
+clear all;
+
+sayi = 5;
+if sayi>5
+    mesaj = 'dogru';
+    sayi = sayi+2;
+end
+
+adim = 'mehmet';
+
+sayi = 6;
+if sayi>5
+    mesaj = 'dogru';
+    sayi = sayi+2;
+end
+
+adim = 'mehmet';
+
+sayi = 5;
+if sayi>=5
+    mesaj = 'dogru';
+    sayi = sayi+2;
+end
+
+adim = 'mehmet';
+
+sayi2 = 7;
+sayi3 = 0;
+
+if sayi3
+    kelime ='heh';
+end
+
+if sayi2
+    kelime ='peh';
+end
