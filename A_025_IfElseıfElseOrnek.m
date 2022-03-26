5f21d4746e359fa858623b44d1284ed49218e3b2
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-26 10:01:43 +0300

    Create A_025_IfElseıfElseOrnek.m

---
+++ A_025_IfElseıfElseOrnek.m
@@ -0,0 +1,15 @@
+clc;
+clear all;
+not = 3;
+if not == 5
+    mesaj = 'çok iyi çalışma'
+elseif not  == 4
+    mesaj = 'iyi çalışma'
+elseif not == 3
+    mesaj = 'ortlama çalışma'
+    not =2;
+elseif not == 2
+    mesaj = 'daha iyi çalış'
+else
+    mesaj = 'çalış'
+end 
