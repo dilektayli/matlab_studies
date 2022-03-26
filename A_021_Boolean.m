85cf2304fc146178d03e3581e33a782677209bab
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-26 09:16:14 +0300

    Create A_021_Boolean.m

---
+++ A_021_Boolean.m
@@ -0,0 +1,15 @@
+clear all;
+clc;
+deger1 = true;
+deger2 = false;
+
+deger3 = 999;
+deger4 = logical(999);
+deger5 = logical(-999);
+deger6 = logical(0);
+
+deger7 = [1,2,3,4;5,6,7,8];
+deger8 = logical(deger7);
+
+deger2 = deger1+deger2;
+deger8 = deger1+deger2
