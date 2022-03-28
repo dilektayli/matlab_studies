8955008223ef40069858e41b6551d4e35d29f832
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-28 23:58:50 +0300

    Create A_041_switch.m

---
+++ A_041_switch.m
@@ -0,0 +1,49 @@
+ay = input("ay girin: ");
+ 
+switch ay
+    case 1
+        disp("ocak ");
+    case 2
+        disp("şubat ");
+    case 3
+        disp("mart ");
+    case 4
+        disp("nisan ");
+    case 5
+        disp("mayıs ");
+    case 6
+        disp("haziran ");
+    case 7
+        disp("temmuz ");
+    case 8
+        disp("ağustos ");
+    case 9
+        disp("eylül ");
+    case 10
+        disp("ekim ");
+    case 11
+        disp("kasım ");
+    case 12
+        disp("aralık ");
+    otherwise
+        disp("tekrar dene")
+end 
+
+
+%%
+not = input("not girin: 1-5 ");
+ 
+switch not
+    case 1
+        disp("olacakkkkkkkk ");
+    case 2
+        disp("olacakkkk ");
+    case 3
+        disp("olucakk ");
+    case 4
+        disp("oluyorr ");
+    case 5
+        disp("olmuşşş ");
+    otherwise
+        disp("tekrar dene")
+end 
