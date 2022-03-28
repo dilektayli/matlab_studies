41e54edf02809dd6a72768e0773bd20ca0805501
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-03-28 22:36:59 +0300

    Create A_035_for.m

---
+++ A_035_for.m
@@ -0,0 +1,19 @@
+clc;
+clear all;
+close all;
+
+for i= 1:1:5
+    disp("hey")
+end
+
+for i= 1:10
+    fprintf("%d\n",i)
+end
+
+for i=10:-1:11
+    fprintf("%d\n",i)
+end
+
+for i=10:-1:1
+    fprintf("%d\n",i)
+end
