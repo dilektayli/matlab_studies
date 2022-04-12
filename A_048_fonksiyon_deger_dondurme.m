35c264481052279a31be68e9927374c8ffe6a788
kdxxx <83818961+kdxxx@users.noreply.github.com>
2022-04-13 00:10:10 +0300

    Update A_048_fonksiyon_deger_dondurme.m

--- A_048_fonksiyon_deger_dondurme.m
+++ A_048_fonksiyon_deger_dondurme.m
@@ -2,3 +2,10 @@ clc;
 clear;
 sayilar = input('sayilari girin: ');
 sonuclar = kontrolEt(sayilar)
+
+
+degerler = input('sayilari girin: ');
+sonuclar2 = asalSayiBul(degerler);
+
+degerler3 = input('sayilari girin: ');
+[p n] = ayir(degerler)
